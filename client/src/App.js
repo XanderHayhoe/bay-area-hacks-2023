d4b7849d2f0e5ed3b380939bf6ade3116cd21e31
XanderHayhoe <xanderhayhoe@gmail.com>
2023-02-18 13:33:46 -0500

    create repo

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import Home from "./routing/Home/Home";
+import Navbar from "./routing/Navbar/Navbar";
+function App() {
+  return (
+    <div className='App'>
+      <Navbar />
+      <Home />
+    </div>
+  );
+}
+
+export default App;
